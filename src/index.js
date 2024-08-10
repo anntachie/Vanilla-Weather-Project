f04725b18fdfd02c8f04e5aad36f2c83913c3738
Ann <anntachie@gmail.com>
2024-08-09 23:40:05 -0800

    added more weather data

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 function displayTemperature(response)
 {let temperatureElement = document.querySelector("#current-temperature");
     let cityElement = document.querySelector("h1");
+    let windElement = document.querySelector("#wind");
+    let humidityElement = document.querySelector("#humidity");
+    let descriptionElement = document.querySelector("#description");
     let temperature = Math.round(response.data.temperature.current);
     cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
     temperatureElement.innerHTML= temperature;
+    windElement.innerHTML= `${response.data.wind.speed}km/h`;
+    humidityElement.innerHTML= `${response.data.temperature.humidity}%`;
+    
 }
 
-function search(event){
-    event.preventDefault();
-    let searchInputElement= document.querySelector("#search-input");
-    let city =  searchInputElement.value;
-    let apiKey = "8c3t8b68c39aceaa7e74616od00b0ef1";
+function searchCity(city) {
+     let apiKey = "8c3t8b68c39aceaa7e74616od00b0ef1";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayTemperature);
+}
+
+
+function searchSubmit(event){
+    event.preventDefault();
+    let searchInput= document.querySelector("#search-input");
+ searchCity(searchInput.value)
+   
 }
 
 function formatDate(date)
@@ -35,7 +47,8 @@ let days=[
 }
 
 let searchForm = document.querySelector("#search-form");
-    searchForm.addEventListener("submit", search);
+    searchForm.addEventListener("submit", searchSubmit);
+    searchCity("Accra");
 
     let currentDateElement= document.querySelector("#current-date");
     let currentDate =  new Date();
